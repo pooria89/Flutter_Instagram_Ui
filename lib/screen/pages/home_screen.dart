23f849cb38fa66212a21283251d86d136b647fb7
pooria <pooria.maleki89@yahoo.com>
2022-07-04 13:39:57 +0430

    [FEATURE] Timeline Ui Completed .

--- lib/screen/pages/home_screen.dart
+++ lib/screen/pages/home_screen.dart
@@ -4,15 +4,39 @@ import '../../widget/main/bubble_stories.dart';
 import '../../widget/main/user_post.dart';
 
 class HomeScreen extends StatelessWidget {
-  final List people = ["pooria", "qazal", "mamad", "alireza", "leila", "saba"];
+  final List people = [
+    "test1",
+    "test2",
+    "test3",
+    "test4",
+    "test5",
+    "test6",
+    "test7",
+    "test8",
+    "test9",
+    "test10"
+  ];
+
+  final List<String> imagesList = [
+    "https://cdn.pixabay.com/photo/2020/11/01/23/22/breakfast-5705180_1280.jpg",
+    "https://images.wallpaperscraft.com/image/single/coffee_beans_coffee_cup_198851_1920x1080.jpg",
+    "https://cdn.pixabay.com/photo/2016/11/18/19/00/breads-1836411_1280.jpg",
+    "https://cdn.pixabay.com/photo/2017/04/04/18/07/ice-cream-2202561_1280.jpg",
+    "https://cdn.pixabay.com/photo/2020/11/01/23/22/breakfast-5705180_1280.jpg",
+    "https://cdn.pixabay.com/photo/2016/11/18/19/00/breads-1836411_1280.jpg",
+    "https://images4.alphacoders.com/895/thumb-1920-895296.jpg",
+    "https://img.freepik.com/free-photo/wooden-table-with-cup-coffee_1203-1631.jpg?w=1380&t=st=1656925514~exp=1656926114~hmac=e51bf2786df625542db5aa9b406769b1e0f0d2d255787153e5df9e2e29248d6f",
+    "https://cdn.pixabay.com/photo/2019/01/14/17/25/gelato-3932596_1280.jpg",
+    "https://img.freepik.com/free-photo/close-up-hands-barista-make-latte-coffee-art-paint_1150-12161.jpg?w=1380&t=st=1656925517~exp=1656926117~hmac=d49295f5889876fa79e8db74cac25c0a279065be9f5966156d1ff1513cdd8c8c"
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        backgroundColor: Colors.grey[300],
-        elevation: 0.0,
+        backgroundColor: Colors.grey[100],
+        elevation: 1.0,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -38,9 +62,16 @@ class HomeScreen extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 itemCount: people.length,
                 itemBuilder: (context, index) {
-                  return BubbleStories(
-                    username: "pooria",
-                    url: "https://wallpaperaccess.com/full/3174207.jpg",
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                    child: Row(
+                      children: [
+                        BubbleStories(
+                          username: people[index],
+                          url: imagesList[index],
+                        ),
+                      ],
+                    ),
                   );
                 }),
           ),
