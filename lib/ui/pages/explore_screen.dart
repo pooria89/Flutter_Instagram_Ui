bf33048647b4c30fcfc4553b2799d54a58d973e3
pooria <pooria.maleki89@yahoo.com>
2022-08-09 09:23:02 +0430

    clean code .

--- lib/ui/pages/explore_screen.dart
+++ lib/ui/pages/explore_screen.dart
@@ -3,28 +3,28 @@ import 'package:pooria50/widget/explore/category_bar.dart';
 import 'package:pooria50/widget/explore/custom_explore_appbar.dart';
 import 'package:pooria50/widget/explore/persistent_header.dart';
 
-class ExploreScreen extends StatelessWidget {
-  const ExploreScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: SafeArea(
-          child: CustomScrollView(
-              slivers: [
-              CustomExploreAppbar(),
-          SliverPersistentHeader(
-            pinned: true,
-            delegate: PersistentHeader(child: CategoryBar(
-              [
-                "Blogger",
-                "Sport",
-                "Health"
-              ]
-            ),))
-            ],
-          )),
-    );
-  }
-}
+// class ExploreScreen extends StatelessWidget {
+//   const ExploreScreen({Key? key}) : super(key: key);
+  //
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     color: Colors.white,
+  //     child: SafeArea(
+  //         child: CustomScrollView(
+  //             slivers: [
+  //             CustomExploreAppbar(),
+  //         SliverPersistentHeader(
+  //           pinned: true,
+  //           delegate: PersistentHeader(child: CategoryBar(
+  //             [
+  //               "Blogger",
+  //               "Sport",
+  //               "Health"
+  //             ]
+  //           ),))
+  //           ],
+  //         )),
+  //   );
+  // }
+// }
